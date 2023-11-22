d49f221ea0f11dba269825abe77a493bc292b90d
TechByKen <diala.kenneth98@gmail.com>
2023-11-22 06:16:26 -0800

    Updated the script file

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("mouseover", (e) => {
-  console.log(e.pageX);
+  const btnX = e.pageX - btn.offsetLeft;
+  const btnY = e.pageY - btn.offsetTop;
+
+  btn.style.setProperty("--xPos", btnX + "px");
+  btn.style.setProperty("--yPos", btnY + "px");
 });
